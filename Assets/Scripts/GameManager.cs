fac1dbf6040ccef2c79640cc86d896796beb3dd5
SooHyun3Lee <43182939+SooHyun3Lee@users.noreply.github.com>
2020-04-04 22:26:24 +0900

    Initialized Unity project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                m_instance = FindObjectOfType<GameManager>();
+            }
+            return m_instance;
+        }
+    }
+
+    private static GameManager m_instance;
+
+    // Define required variables
+
+    private void Awake()
+    {
+        if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
