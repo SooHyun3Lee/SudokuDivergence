3552faf53f39eb5e5993473833e5a5e6bbfbff8b
SooHyun3Lee <43182939+SooHyun3Lee@users.noreply.github.com>
2020-04-05 22:26:45 +0900

    Changing to checking validity of solution after each input

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 public class BoardManager : MonoBehaviour
 {
     private bool isFilled = false;
-    private bool isCorrect = false;
+    //private bool isCorrect = false;
 
     //private Tile[,] masterBoard = new Tile[9,9]; //9x9 array of Tiles
     private Tile[] masterBoard = new Tile[81]; //9x9 array of Tiles
+
+    /* when the player inputs a number, add it to corresponding row, col, and 
+     * box List to be used for duplicate checking later on */
+    private List<int>[] rows = new List<int>[9];
+    private List<int>[] cols = new List<int>[9];
+    private List<int>[] boxes = new List<int>[9];
+
+    /* determine which List to access by using division and mod
+     * i.e.) (pos / 9) , (pos mod 9) to get (row, col) coordinate
+     */
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+        {
+
+        }
+    }
 
     // if isFilled && isCorrect, sudoku cleared
 }
--- Assets/Scripts/ClassicSudoku.cs
+++ Assets/Scripts/ClassicSudoku.cs
@@ -7,7 +7,7 @@ public class ClassicSudoku : MonoBehaviour
     // need to consider fixing list size for the sake of performance
     public List<int[]> rules;
 
-    void Start()
+    /*void Start()
     {
         rules.Add(new int[9] { 0, 1, 2, 3, 4, 5, 6, 7, 8 });
         rules.Add(new int[9] { 9, 10, 11, 12, 13, 14, 15, 16, 17 });
@@ -38,7 +38,7 @@ public class ClassicSudoku : MonoBehaviour
         rules.Add(new int[9] { 60, 61, 62, 69, 70, 71, 78, 79, 80 });
     }
 
-    /*int[] row1 = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+    int[] row1 = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
     int[] row2 = { 9, 10, 11, 12, 13, 14, 15, 16, 17 };
     int[] row3 = { 18, 19, 20, 21, 22, 23, 24, 25, 26 };
     int[] row4 = { 27, 28, 29, 30, 31, 32, 33, 34, 35 };
